22acdfee556d758447ea8c21d9a0255ad6d9ab47
Afaq <afaaaq85@gmail.com>
2025-11-17 10:15:51 +0500

    fix: routes navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -8,7 +8,15 @@ import { adminInterceptor } from './interceptor/admin.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',  // 👈 scroll to top on navigation
+        anchorScrolling: 'enabled',        // 👈 enable #section scrolling
+      })
+    ),
+
     provideHttpClient(withInterceptors([adminInterceptor])),
   ],
 };
